a7583024df9729c6524f9dedc43ac13a3690f3f3
santhoshmurali <santhoshmurali@gmail.com>
2024-11-02 16:41:55 +0530

    container

---
+++ main.py
@@ -0,0 +1,18 @@
+import yaml
+
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def main():
+    config = load_config('config.yaml')
+    message = config.get('message','hello docker')
+
+    print(f"message from config : { message }")
+
+
+if __name__ == '__main__':
+    main()
+        
+    
